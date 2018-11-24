38652cda0df7b1f13e02a17d9bc1c22b0c374915
Leonardo <41441108+Peskyleo1@users.noreply.github.com>
2018-11-24 14:04:08 +0100

    Add files via upload

--- Episode 1/Scripts/script.js
+++ Episode 1/Scripts/script.js
@@ -1,2 +1,30 @@
-console.log("Helllo");
-console.log("Heyyy");
+//Hello and welcome to Episode 1
+
+//This is a comment. Your computer will ignore it.
+//Specifically these are all "single line" comments.
+//They are used usually when a developer wants to describe parts of their code.
+//Single line comments start with the " // " symbols and only include whats after them on the same line
+//Multi line comments start with " /* " and end with " */ ". They include everything in between them.
+
+//Please save the World with a multi line comment by commenting lines 11 to 24:
+
+
+"console.log()" will take "any" kind of data type.
+
+--------------------------------------------------
+
+There are different kinds of data types,
+for now all we need to know is: 
+
+• Strings: this is just simple text. The computer understands you want a
+           string only when you put it between "speechmarks"
+
+• Intergers: An integer is a whole number (not a fraction / no decimals) that can be 
+             positive, negative, or zero.
+
+
+
+
+
+
+//Jameson is very sexy
